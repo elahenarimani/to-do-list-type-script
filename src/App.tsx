cf2e93044b76b1b2444aa2e936b25c648e3feff8
elahe01~ <elahe.narimani@gmail.com>
2024-01-01 21:47:19 +0330

    complete css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { VscChecklist } from "react-icons/vsc";
+import { CiSearch } from "react-icons/ci";
+import { FaFilter } from "react-icons/fa6";
+import { TbPencilPlus } from "react-icons/tb";
+import TableMobile from './components/tableMobile/TableMobile';
+import TableDesktop  from './components/tableDesktop/TableDesktop';
+import Aaaa from './components/aaaa/Aaaa';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='w-full h-[50px] flex justify-between items-center gap-[20px] bg-[#6200EA] pl-[10px] pr-[10px] text-[#FFFFFF]'>
+        <div className='min-w-[80px] flex justify-between items-center gap-[5px]'>
+          <div>
+            <VscChecklist />
+          </div>
+          <div>
+            <p className='truncate min-w-[100px]'> My To-Do Tasks </p>
+          </div>
+        </div>
+        <div className='max-w-[80%]  flex justify-between items-center gap-[10px]  bg-[#6200EA]'>
+          <div className='search-wrapper  max-w-[200px] flex justify-between items-center  border-[2px] border-gray-400 border-solid rounded-[5px] pl-[7px] pr-[7px]' >
+            <input className='w-full h-full bg-[#6200EA] border-none outline-none' placeholder='Search'/>
+            <div className=''><CiSearch /></div>
+          </div>
+          <div>
+            <FaFilter />
+          </div>
+          <div>
+            <TbPencilPlus />
+          </div>
+        </div>
       </header>
+      <main className='w-full h-full'>
+        {/* <TableMobile/> */}
+        {/* <TableDesktop/> */}
+       <Aaaa/>
+      </main>
     </div>
   );
 }
